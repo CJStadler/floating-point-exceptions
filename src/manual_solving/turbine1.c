8a02f371ba0b82b79de603ce4829036959aac80a
Chris Stadler <chrisstadler@gmail.com>
2019-10-16 10:28:41 -0400

    Add main function

--- src/manual_solving/turbine1.c
+++ src/manual_solving/turbine1.c
@@ -7,3 +7,19 @@ double ex6(double v, double w, double r) {
   return (((3.0 + (2.0 / (r * r))) - (((0.125 * (3.0 - (2.0 * v))) * (((w * w) * r) * r)) / (1.0 - v))) - 4.5);
 }
 
+int main(int argc, char** argv) {
+  if (argc < 4) {
+    puts("3 inputs required");
+    exit(1);
+  }
+
+  double v = atof(argv[1]);
+  double w = atof(argv[2]);
+  double r = atof(argv[3]);
+
+  double res = ex6(v, w, r);
+
+  printf("ex6(%.10e, %.10e, %.10e) = %.10e\n", v, w, r, res);
+
+  return 0;
+}
