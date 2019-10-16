0662cf711e1bdd0ab88ae5bdee3fcc5920687c03
Chris Stadler <chrisstadler@gmail.com>
2019-10-16 10:52:33 -0400

    Instrument first operation

--- src/manual_solving/turbine1.c
+++ src/manual_solving/turbine1.c
@@ -3,8 +3,36 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+// We can't actually call this because then it would only need to be reached one
+// for coverage. Can it be a macro?
+double check_mult(double a, double b) {
+  double product = a * b;
+  double abs1 = fabs(product);
+  if (DBL_MAX < abs1) {
+    fprintf(stderr, "Overflow!\n");
+    exit(1);
+  }
+  if (0 < abs1 && abs1 < DBL_MIN) {
+    fprintf(stderr, "Underflow!\n");
+    exit(1);
+  }
+
+  return product;
+}
+
 double ex6(double v, double w, double r) {
-  return (((3.0 + (2.0 / (r * r))) - (((0.125 * (3.0 - (2.0 * v))) * (((w * w) * r) * r)) / (1.0 - v))) - 4.5);
+  double t1 = r * r;
+  double abs1 = fabs(t1);
+  if (DBL_MAX < abs1) {
+    fprintf(stderr, "Overflow!\n");
+    exit(1);
+  }
+  if (0 < abs1 && abs1 < DBL_MIN) {
+    fprintf(stderr, "Underflow!\n");
+    exit(1);
+  }
+
+  return (((3.0 + (2.0 / (t1))) - (((0.125 * (3.0 - (2.0 * v))) * (((w * w) * r) * r)) / (1.0 - v))) - 4.5);
 }
 
 int main(int argc, char** argv) {
