c8a88a7e48e7ba1945fdc7cc2b01a18a3f0d41ee
Chris Stadler <chrisstadler@gmail.com>
2019-11-21 09:55:02 -0500

    Make trace type and start refactor

--- src/search/driver.cpp
+++ src/search/driver.cpp
@@ -3,71 +3,19 @@
 #include <float.h>
 #include <fenv.h>
 #include <random>
+#include <vector>
 
-#include "../exception_counters.h"
+#include "../fp_exception.hpp"
+
+extern ExceptionTrace last_trace;
 
 // Interfaces for P and P'
-double p_unopt(double input);
-double p_opt(double input);
+double p_unopt(std::vector<double>* args);
+double p_opt(std::vector<double>* args);
 
-const double SEED = 59.3;
-const int ITERATIONS = 1000000000;
-const double INPUT_MAX = 1.0;
-const double INPUT_MIN = -INPUT_MAX;
-
-int check_exceptions() {
-  // Return the total number of exceptions
-  int total = overflows
-            + underflows
-            + divbyzeros
-            + invalids;
-  reset_counts();
-  return total;
-}
-
-int main() {
-  std::default_random_engine generator(SEED);
-  std::uniform_real_distribution<double> distribution(INPUT_MIN, INPUT_MAX);
-
-  double input;
-
-  int only_unopt = 0;
-  int only_opt = 0;
-  int both = 0;
-  int neither = 0;
-
-  printf("Running %i iterations\n", ITERATIONS);
-
-  for (int i = 0; i < ITERATIONS; i++) {
-    input = distribution(generator);
-    reset_counts();
-    feclearexcept(FE_ALL_EXCEPT);
-    double _r1 = p_unopt(input);
-    int exceptions_unopt = check_exceptions();
-    double _r2 = p_opt(input);
-    int exceptions_opt = check_exceptions();
-
-    if (exceptions_unopt > 0 || exceptions_opt > 0) {
-      fprintf(stderr, "input: %.10e unopt: %i opt: %i\n",
-          input, exceptions_unopt, exceptions_opt);
-    }
-
-    // Increment the appropriate counter.
-    if (exceptions_unopt > 0) {
-      if (exceptions_opt > 0) {
-        both++;
-      } else {
-        only_unopt++;
-      }
-    } else if (exceptions_opt > 0) {
-      only_opt++;
-    } else {
-      neither++;
-    }
-  }
-
-  printf("%i had no exceptions\n", neither);
-  printf("%i had exceptions in both\n", both);
-  printf("%i had exceptions only for opt\n", only_opt);
-  printf("%i had exceptions only for unopt\n", only_unopt);
+int main(int argc, char* argv[]) {
+  double _r1 = p_unopt(input);
+  ExceptionTrace trace_opt = last_trace;
+  double _r2 = p_opt(input);
+  ExceptionTrace trace_unopt = last_trace;
 }
