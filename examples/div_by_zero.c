38b392de17b1161522f494bdc7f8f50030c1066c
Chris Stadler <chrisstadler@gmail.com>
2019-12-16 13:47:11 -0500

    Remove old examples

---
+++ examples/div_by_zero.c
@@ -0,0 +1,4 @@
+double div_by_zero(double x) {
+    double y = 0.0;
+    return x / y;
+}
--- examples/nan.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-
-int main() {
-    double x = -0.0;
-    double y = 0.0;
-    double nnan = x / y;
-    int result = nnan == 0.0;
-    printf("%d\n", result);
-}
